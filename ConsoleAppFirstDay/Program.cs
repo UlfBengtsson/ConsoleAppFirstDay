2255dee15d5cfe70e306c665ac20505230b9b79b
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-05-27 14:19:50 +0200

    Add project files.

---
+++ ConsoleAppFirstDay/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppFirstDay
+{
+    /*
+      Comment block
+      alla lines int this are comments
+     */
+
+    class Program
+    {
+        // This is a comment line
+        static void Main(string[] args)
+        {
+            string name = "Ulf";
+
+            Console.WriteLine("Hello! this is " + name + " first code in C#");
+        }
+    }
+}
